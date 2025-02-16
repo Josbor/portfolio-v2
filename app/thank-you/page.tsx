3d6ef965201da9451b381864e834d30fb5df9c4d
Jose Borrego <josbor.dev@gmail.com>
2025-02-16 10:18:50 -0400

    feat: replace hardcoded data with JSON
    - Updated `app/data/content.json` to include new data structure.
    - Modified `app/thank-you/page.tsx` to fetch and display data from JSON.

--- app/thank-you/page.tsx
+++ app/thank-you/page.tsx
@@ -7,47 +7,34 @@ import { useLanguage } from "../contexts/LanguageContext";
 import { use, useContext, useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import { Providers } from "../providers";
+import content from "../data/content.json";
 
 export default function ThankYouPage() {
   const { language } = useLanguage();
   const [storedLanguage, setLanguage] = useState<"es"|"en">("en");
   useEffect(() => {
-    setLanguage((localStorage.getItem("language") ?? language)as "es" | "en");
+    setLanguage((localStorage.getItem("language") ?? language) as "es" | "en");
   },[])
-  const content={
-    es:{
-      title: "¡Gracias por contactar!",
-      description: "Hemos recibido tu mensaje. me pondre en contacto contigo pronto.",
-      buttonText: "Volver a la página principal"
-    },
-    en:{
-      title: "Thank you for contacting!",
-      description: "We have received your message. I will contact you soon.",
-      buttonText: "Back to the main page"
-    }
-  }
   return (
-     
     <div className="min-h-screen bg-gradient-to-b flex items-center justify-center px-4">
       <Card className="max-w-md w-full">
         <CardHeader>
           <div className="mx-auto mb-4">
             <CheckCircle className="h-16 w-16 text-green-500" />
           </div>
-          <CardTitle className="text-center">{content[storedLanguage].title}</CardTitle>
+          <CardTitle className="text-center">{content.thankYou[storedLanguage].title}</CardTitle>
           <CardDescription className="text-center">
-            {content[storedLanguage].description}
+            {content.thankYou[storedLanguage].description}
           </CardDescription>
         </CardHeader>
         <CardContent>{/* You can add additional content here if needed */}</CardContent>
         <CardFooter className="flex justify-center">
           <Button asChild>
-            <Link href="/">{content[storedLanguage].buttonText }</Link>
+            <Link href="/">{content.thankYou[storedLanguage].buttonText}</Link>
           </Button>
         </CardFooter>
       </Card>
     </div>
-    
   )
 }
 
